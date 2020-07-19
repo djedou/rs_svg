fd83718a6f41dd06b7ceb0cf2f12c6135a1649f1
djedou <djedou@localhost.localdomain>
2020-07-19 15:26:15 +0200

    fix line attr

--- src/elements/shapes/line/line_attributes.rs
+++ src/elements/shapes/line/line_attributes.rs
@@ -9,44 +9,44 @@ pub struct LineProps {
 
     // own Attribute
     /// Defines the x-axis coordinate of the line starting point.
-    #[prop_or(None)]
+    #[prop_or_default]
     pub x1: Option<String>,
     /// Defines the x-axis coordinate of the line ending point.
-    #[prop_or(None)]
+    #[prop_or_default]
     pub x2: Option<String>,
     /// Defines the y-axis coordinate of the line starting point.
-    #[prop_or(None)]
+    #[prop_or_default]
     pub y1: Option<String>,
     /// Defines the y-axis coordinate of the line ending point.
-    #[prop_or(None)]
+    #[prop_or_default]
     pub y2: Option<String>,
     /// Defines the total path length in user units.
-    #[prop_or(None)]
+    #[prop_or_default]
     pub path_length: Option<String>,
 
     // Core
     /// assigns a unique name to an element.
-    #[prop_or(None)]
+    #[prop_or_default]
     pub id: Option<String>,
     /// allows you to control whether an element is focusable and to define 
     /// the relative order of the element for the purposes of sequential focus navigation.
-    #[prop_or(None)]
+    #[prop_or_default]
     pub tabindex: Option<String>,
 
     // Styling 
     /// allows to style an element using CSS declarations.
-    #[prop_or(None)]
+    #[prop_or_default]
     pub style: Option<String>,
     /// Assigns a class name or set of class names to an element.
-    #[prop_or(None)]
+    #[prop_or_default]
     pub class: Option<String>,
 
 
-    #[prop_or(None)]
+    #[prop_or_default]
     pub stroke: Option<String>,
-    #[prop_or(None)]
+    #[prop_or_default]
     pub stroke_width: Option<String>,
-    #[prop_or(None)]
+    #[prop_or_default]
     pub fill: Option<String>
 
     // Global Event
