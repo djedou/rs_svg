3c7a9a8caed07fcc95609f4b8b48a61069f3165e
djedou <djedou@localhost.localdomain>
2020-07-19 11:00:40 +0200

    remove attr

--- src/elements/shapes/line/line_attributes.rs
+++ src/elements/shapes/line/line_attributes.rs
@@ -6,29 +6,29 @@ use serde::Serialize;
 
 #[derive(Clone, PartialEq, Properties, Serialize)]
 pub struct LineProps {
-    #[prop_or_default]
+    //#[prop_or_default]
     pub x1: String,
-    #[prop_or_default]
+    //#[prop_or_default]
     pub x2: String,
-    #[prop_or_default]
+    //#[prop_or_default]
     pub y1: String,
-    #[prop_or_default]
+    //#[prop_or_default]
     pub y2: String,
-    #[prop_or_default]
+    //#[prop_or_default]
     pub path_length: String,
 
     /// Core Attribute
-    #[prop_or_default]
+    //#[prop_or_default]
     pub id: String,
     /// Core Attribute
-    #[prop_or_default]
+    //#[prop_or_default]
     pub tabindex: String,
-    #[prop_or_default]
+    //#[prop_or_default]
     pub style: String,
-    #[prop_or_default]
+    //#[prop_or_default]
     pub stroke: String,
-    #[prop_or_default]
+    //#[prop_or_default]
     pub stroke_width: String,
-    #[prop_or_default]
+    //#[prop_or_default]
     pub fill: String
 }
